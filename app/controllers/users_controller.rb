7a194f9ebb3f26a272118be20b804643c3b7eeab
Jon Kaplon <jon.kaplon1@gmail.com>
2022-04-06 13:48:57 -0700

    commit tree 2a81308810c03ef97be8aea69dad534f2a62052b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :not_found
   end
 
 end
